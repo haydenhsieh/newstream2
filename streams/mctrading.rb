e51467a9dea521e0d74824256fc50c2fb3b9936a
haydenhsieh <haydenhsieh@foo.blah>
2026-01-20 14:19:00 +0800

    Added streams/mctrading.rb.

---
+++ streams/mctrading.rb
@@ -0,0 +1,18 @@
+require_relative 'stream'
+
+class Mctrading < Stream
+  def parse(web)
+    feeds = []
+    dates = web.find_elements(xpath: "//span[contains(@class, 'post-metadata__date')]").map(&:text)
+    titles = web.find_elements(xpath: "//div[contains(@data-hook, 'post-title')]")
+    dates.zip(titles).each do |d, t|
+      feeds << {
+        stream: self.class.name,
+        title: t,
+        date: d,
+        url: @url
+      }
+    end
+    return {feeds: feeds}
+  end
+end
