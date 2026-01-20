c19ddbdaa0795efceafc98471d0a59c457a81266
haydenhsieh <haydenhsieh@foo.blah>
2026-01-20 17:04:15 +0800

    Fixed stream/mctrading.rb title.

--- streams/mctrading.rb
+++ streams/mctrading.rb
@@ -4,7 +4,7 @@ class Mctrading < Stream
   def parse(web)
     feeds = []
     dates = web.find_elements(xpath: "//span[contains(@class, 'post-metadata__date')]").map(&:text)
-    titles = web.find_elements(xpath: "//div[contains(@data-hook, 'post-title')]")
+    titles = web.find_elements(xpath: "//div[contains(@data-hook, 'post-title')]").map(&:text)
     dates.zip(titles).each do |d, t|
       feeds << {
         stream: self.class.name,
