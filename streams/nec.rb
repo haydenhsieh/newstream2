ee19d45d586f7e265b594c69451513b17aec8cf7
haydenhsieh <haydenhsieh@foo.blah>
2026-01-20 12:27:18 +0800

    Add streams/nec.rb.

---
+++ streams/nec.rb
@@ -0,0 +1,18 @@
+require_relative 'stream'
+
+class Nec < Stream
+  def parse(web)
+    feeds = []
+    dates  = web.find_elements(xpath: "//div[@class='mod-list-news of-type-02']//div[@class='row']/dt").map(&:text)
+    titles = web.find_elements(xpath: "//div[@class='mod-list-news of-type-02']//div[@class='row']/dd").map(&:text)
+    dates.zip(titles).each do |d, t|
+      feeds << {
+        stream: self.class.name,
+        title: t,
+        date:  d,
+        url:  @url
+      }
+    end
+    return {feeds: feeds}
+  end
+end
