5b85211cfeffbd1c8434da73abdc34d5533a6e7d
haydenhsieh <haydenhsieh@foo.blah>
2026-01-21 12:31:42 +0800

    Update test_stream.rb by adding --config FILE

--- test/test_stream.rb
+++ test/test_stream.rb
@@ -12,20 +12,29 @@ if __FILE__ == $PROGRAM_NAME
   require_relative File.join(dir, "streams/stream")
   require 'selenium-webdriver'
   require 'json'
+  require 'optparse'
+  options = { config: "config.json" }
+  OptionParser.new do |opt|
+    opt.on("-c", "--config CONF_FILE")
+  end.parse!(into: options)
 
-  config = JSON.load_file(File.join(dir, "config.json"))
-  stream_config = config["streams"]
+  config = JSON.load_file(File.join(dir, options[:config]), symbolize_names: true)
+  stream_config = config[:streams]
 
   options = Selenium::WebDriver::Options.firefox
   options.args << "--headless=new"
   web = Selenium::WebDriver.for :firefox, options: options
 
   ARGV.each do |name|
-    target = stream_config[name]
+    target = stream_config[name.to_sym]
     if target
       require_relative File.join(dir, "streams/#{name}")
-      stream = Object.const_get(name.camelize).new(url: target["url"])
-      web.get(stream.url)
+      stream = Object.const_get(name.camelize).new(**target)
+      if stream.url
+        web.get(stream.url)
+      else
+        stream.get(web)
+      end
       result = stream.parse(web)
       puts result
       web.quit
